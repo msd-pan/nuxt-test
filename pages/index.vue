2f9eebdc6e2b8788e8e36ff3090407ea87a6abbd
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-04-03 13:48:23 +0900

    Section 2: Basic concepts of Nuxt.js => Pages & routing

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,12 @@
-<template><div></div></template>
+<template>
+  <nuxt-link to="/about">About</nuxt-link>
+  <br />
+  <nuxt-link to="/notes" target="_blank">Notes</nuxt-link>
+  <br />
+  <nuxt-link :to="`/notes/${randomNumber()}`">Notes</nuxt-link>
+  <h1>hello world</h1>
+</template>
+<script setup>
+const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+</script>
+<style></style>
---
+++ pages/notes/index.vue
@@ -0,0 +1,1 @@
+<template><h1>Notes</h1></template>
