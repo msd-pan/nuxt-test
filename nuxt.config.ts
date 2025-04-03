3e37386f0358b0f98fc8d46c2fe0fd2ea0f97b62
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-04-03 16:24:10 +0900

    Middleware and its types

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import type { NuxtPage } from "nuxt/schema";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
@@ -8,4 +10,19 @@ export default defineNuxtConfig({
       pathPrefix: false,
     },
   ],
+
+  hooks: {
+    "pages:extend"(pages) {
+      const setMiddleware = (pages: NuxtPage[]) => {
+        for (const page of pages) {
+          if (page.path === "/about") {
+            page.meta ||= {};
+            page.meta.middleware = ["named"];
+          }
+        }
+      };
+
+      setMiddleware(pages);
+    },
+  },
 });
