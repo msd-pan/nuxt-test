3a0a802729cd4bf571bc0c5af42aba579f55a772
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-04-07 14:09:53 +0900

    Data binding and directives

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,1 +1,9 @@
-<template><h1>Welcome To Our Notes App!</h1></template>
+<template>
+  <h1 v-debug>Welcome To Our Notes App!</h1>
+  <!-- <img :src="imgSrc" alt="" /> -->
+</template>
+<script setup>
+// const imgSrc = ref(
+//   "https://cdn.pixabay.com/photo/2021/12/04/20/59/animal-6845972_640.jpg"
+// );
+</script>
--- components/Notes/NewNote.vue
+++ components/Notes/NewNote.vue
@@ -1,1 +1,12 @@
-<template><input type="text" placeholder="type new note..." /></template>
+<template>
+  <input v-model="newNote" v-focus type="text" placeholder="type new note..." />
+</template>
+<script setup>
+const newNote = ref("this is pre written value~");
+
+const vFocus = {
+  mounted: (el) => {
+    el.focus();
+  },
+};
+</script>
