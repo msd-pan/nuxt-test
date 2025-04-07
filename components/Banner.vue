8d9614409aa91c75a2e9c9f2fb78abbfac4fed4e
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-04-07 15:10:32 +0900

    Methods and computed properties

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,9 +1,20 @@
 <template>
   <h1 v-debug>Welcome To Our Notes App!</h1>
-  <!-- <img :src="imgSrc" alt="" /> -->
+  <button @click="fullName = 'Carlos cold'">change name</button>
+  <h2>{{ fullName }}</h2>
 </template>
 <script setup>
-// const imgSrc = ref(
-//   "https://cdn.pixabay.com/photo/2021/12/04/20/59/animal-6845972_640.jpg"
-// );
+const firstName = ref("John");
+const lastName = ref("dow");
+
+const fullName = computed({
+  get: () => {
+    return `${firstName.value} ${lastName.value}`;
+  },
+  set: (name) => {
+    const names = name.split(" ");
+    firstName.value = names[0];
+    lastName.value = names[names.length - 1];
+  },
+});
 </script>
