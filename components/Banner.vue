fafc21c5be536b15faaa60f002a19f2375319824
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-04-07 15:43:05 +0900

    Conditional and list rendering

--- components/Banner.vue
+++ components/Banner.vue
@@ -2,6 +2,10 @@
   <h1 v-debug>Welcome To Our Notes App!</h1>
   <button @click="fullName = 'Carlos cold'">change name</button>
   <h2>{{ fullName }}</h2>
+
+  <ul>
+    <li v-for="(value, key) in user" :key="key">{{ key }} : {{ value }}</li>
+  </ul>
 </template>
 <script setup>
 const firstName = ref("John");
@@ -16,5 +20,11 @@ const fullName = computed({
     firstName.value = names[0];
     lastName.value = names[names.length - 1];
   },
+});
+
+const user = reactive({
+  name: "John Doe",
+  age: 30,
+  email: "test@email.com",
 });
 </script>
